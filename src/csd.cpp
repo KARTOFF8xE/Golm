6f56b74814f0f7e07be76acd1716d2f33d8e6e52
KARTOFF8xE <georgmuck1@gmail.com>
2025-01-31 09:44:45 +0100

    controll CSD-vel by website

--- src/csd.cpp
+++ src/csd.cpp
@@ -1,18 +1,22 @@
 #pragma once
 
 #include <Arduino.h>
+
+#include <seesaw_neopixel.h>
+#include "Adafruit_seesaw.h"
+
 #include "config.cpp"
 
 class CSD {
   private:
     int pin;
-    int vel;
+    int speed;
     int dir;
 
   public:
     CSD(int pin) {
       this->pin = pin;
-      this->vel = 0;
+      this->speed = 0;
       this->dir = -1;
     }
 
@@ -26,24 +30,66 @@ class CSD {
       digitalWrite(this->dir, LOW);
     }
 
-    void incVel() {
-      if (this->vel < 255) {
-        analogWrite(dacPin, ++this->vel);
+    void incSpeed() {
+      if (this->speed < 255) {
+        analogWrite(dacPin, ++this->speed);
       }
     }
 
-    void decVel() {
-      if (this->vel > 0) {
-        analogWrite(dacPin, --this->vel);
+    void decSpeed() {
+      if (this->speed > 0) {
+        analogWrite(dacPin, --this->speed);
       }
     }
 
-    void setVel(int32_t newVel) {
-      newVel = constrain(newVel, 0, 255);
-      while (this->vel != newVel) {
-        if (this->vel > newVel) decVel();
-        else incVel();
+    void setSpeed(int32_t newSpeed) {
+      newSpeed = constrain(newSpeed, 0, 255);
+      while (this->speed != newSpeed) {
+        if (this->speed > newSpeed) decSpeed();
+        else incSpeed();
         delay(1);
       }
+    }
+
+    int getSpeed() {
+      return speed;
+    }
+
+    // Takes vel in km/h and rotary-sensor
+    void accelerateToVel(float vel, Adafruit_seesaw ss) {
+        vel = vel / 3.6;
+        ss.setEncoderPosition(0);
+        int rotary_now, rotary_then = ss.getEncoderPosition();
+        int t_now, t_then = 0;
+        do {
+            this->incSpeed();
+            delay(50);
+            t_now = millis();
+        } while(rotary_then == ss.getEncoderPosition());
+        Serial.println();
+
+
+        double vel_current = 0.0;
+        rotary_now = ss.getEncoderPosition();
+        rotary_then = rotary_now;
+        do {
+            if (rotary_now != rotary_then) {
+                t_then = t_now;
+                t_now = millis();
+            
+                this->incSpeed();
+
+                vel_current = (abs(rotary_now - rotary_then) * 3.14 * 10) / ((t_now - t_then));
+                rotary_then = rotary_now;
+            }
+            rotary_now = ss.getEncoderPosition();
+        } while (vel_current <= vel && this->speed < 256);
+    }
+
+    void breakSpeed() {
+        for (speed; speed >= 0; speed--) {
+            this->decSpeed();
+            delay(5);
+        }
     }
 };
